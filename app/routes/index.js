a5783ca5254e15f6b60d445f1b3bcf16085bf34f
johnwickrichman <johnwickrichman@gmail.com>
2022-09-24 13:13:09 +0700

    add "Update database by email" function

--- app/routes/index.js
+++ app/routes/index.js
@@ -49,6 +49,8 @@ router.post('/create', async (req, res) => {
 
 
 
+
+
 /* GET Read user from Database */
 router.get('/read', async (req, res) => {
   try {
@@ -73,6 +75,78 @@ router.get('/read', async (req, res) => {
 
   }
 })
+
+
+
+
+
+
+/* GET Read single user from Database by Email */
+router.get('/read/single/:email', async (req, res) => {
+  
+  let email = req.params.email;
+
+  try {
+
+    let sql = "SELECT * FROM users WHERE email=?";
+
+    conn.query(sql, [email], (err, result) => {
+
+      if (err) {
+        console.log(err);
+        return res.status(400).send();
+      }
+
+      return res.status(200).json(result);
+
+    })
+
+  } catch (e) {
+
+    console.log(e);
+    return res.status(500).send();
+
+  }
+})
+
+
+
+
+
+/* PATCH Update user in Database */
+router.patch('/update/:email', async (req, res) => {
+
+  let email = req.params.email;
+  let newPassword = req.body.newPassword;
+
+  try {
+
+    let sql = "UPDATE users SET password=? WHERE email=?";
+
+    conn.query(sql, [newPassword, email], (err, result) => {
+
+      if (err) {
+        console.log(err);
+        return res.status(400).send();
+      }
+
+      return res.status(200).json({
+        message: "User password updated successfully!"
+      });
+
+    })
+
+  } catch (e) {
+
+    console.log(e);
+    return res.status(500).send();
+
+  }
+} )
+
+
+
+
 
 
 
