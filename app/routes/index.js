866f38105bda7daa5640975535c01b7f3173155e
johnwickrichman <johnwickrichman@gmail.com>
2022-09-24 12:55:20 +0700

    add "Insert to db" function

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,6 +4,9 @@ let mysql = require('mysql');
 
 let conn = require("./connect");
 
+// middleware สำหรับแปลง json object เป็น javascript object
+router.use(express.json());
+
 
 let bodyParser = require('body-parser');
 router.use(bodyParser.json());
@@ -11,11 +14,45 @@ router.use(bodyParser.urlencoded({ extended: true }));
 
 
 
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+
+
+/* GET home page. */
+router.post('/create', async (req, res) => {
+
+  const { name, email, password } = req.body;
+
+  try {
+    let sql = "INSERT INTO users set name=? , email=? , password=?";
+    conn.query(sql, [name, email, password], (err, result, fields) => {
+      if (err) {
+        console.log("Error While insert a user into database", err);
+        return res.status(400).send();
+      }
+
+      return res.status(201).json({
+        message: "New user successfully created!"
+      });
+    })
+
+  } catch (e) {
+
+    console.log(e);
+    return res.status(500).send();
+
+  }
+})
+
+
+
+
+
+
+
 
 
 
